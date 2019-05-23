0f284f83e4d2e5e3c58bb1e99a05aeddcbd34965
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-05-23 15:41:17 +0900

    Add gachonbot.py

---
+++ gachonbot.py
@@ -0,0 +1,20 @@
+﻿import discord
+
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=';;;')
+key = "please_type_a_discord_key"
+
+print('Discord Gachon-Bot Online.')
+
+bot.remove_command('help')
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.send('난 아직 아무것도 못해! 미안!')
+
+@bot.command(name="안녕")
+async def _hi(ctx):
+    await ctx.send('안녕!')
+bot.run(KEY)
